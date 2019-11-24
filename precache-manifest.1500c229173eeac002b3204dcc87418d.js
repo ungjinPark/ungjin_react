08b5c6273d6ca2e27845e383e8f388bedf3c5724
ungjinpark <ungjin312@kakao.com>
2019-11-24 19:18:36 +0900

    Updates

---
+++ precache-manifest.1500c229173eeac002b3204dcc87418d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2c9a254bee7e8032962fcf58451a85",
+    "url": "/ungjin_react/index.html"
+  },
+  {
+    "revision": "c39ac71d7c150ce856e6",
+    "url": "/ungjin_react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4f8f9d9ddc0f2d828f5e",
+    "url": "/ungjin_react/static/js/2.1b417d28.chunk.js"
+  },
+  {
+    "revision": "c39ac71d7c150ce856e6",
+    "url": "/ungjin_react/static/js/main.e2d8d4a5.chunk.js"
+  },
+  {
+    "revision": "a8ccd6f9667233a4b27f",
+    "url": "/ungjin_react/static/js/runtime-main.096ad852.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ungjin_react/static/media/logo.25bf045c.svg"
+  }
+]);
